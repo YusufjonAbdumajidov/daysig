88b6144b142c017b9e4f470b5e5824a232b9e3e6
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-07-02 13:26:01 +0300

    finished the header

--- myapp/src/Components/Navbar.jsx
+++ myapp/src/Components/Navbar.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { BsGlobe2 } from "react-icons/bs";
 
 const Navbar = () => {
+    const [ showDropdown, setShowDropdown ] = useState(false);
+    const [ changeLanguage, setChangeLanguage ] = useState(false)
   return (
     <header>
         <div className="logo">daysig</div>
         <nav>
             <ul>
                 <li>about</li>
-                <li>market</li>
-                <li>community</li>
-                <li>resources</li>
                 <li>signals</li>
-                <li>registration</li>
+                <li onClick={() => setShowDropdown(!showDropdown)} >more</li>
+                <ul className={showDropdown ? "dropDown showDropDown" : 'dropDown'}>
+                  <li>registration</li>
+                  <li>market</li>
+                  <li>community</li>
+                  <li>resources</li>
+                </ul>
+                <li onClick={() => setChangeLanguage(!changeLanguage)} ><BsGlobe2 style={{marginRight: "6px"}}/> {changeLanguage ? "Ru" : "Eng"}</li>
             </ul>
         </nav>
+
+       
     </header>
   )
 }
