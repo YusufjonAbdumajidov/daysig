68decae15af17d8cd1f9846e5f0b0a1d7808bcf0
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-07-04 14:15:08 +0300

    finishing the sponsors section

--- myapp/src/Components/Main.jsx
+++ myapp/src/Components/Main.jsx
@@ -13,8 +13,19 @@ const Main = () => {
             <p>Our team of seasoned traders and cutting-edge algorithms analyze market trends and indicators to deliver accurate signals in real-time.</p>
             <button>Get free signals  <BsArrowRight className='rightArrow' /> </button>
             <div className="sponsors">
-              <div className="binance"><SiBinance /></div>
-              <div></div>
+              <div>
+                <p className="binance"><SiBinance className='sponsorIcon' />Binance</p>
+                <p>Blockchain</p>
+              </div>
+              <div>
+                <p> <SiCoinmarketcap className='sponsorIcon' />Coinmarket</p>
+                <p>Principal</p>
+              </div>
+              <div>
+                <p>Coinbase</p>
+                <p><span>ECN</span>capital</p>
+              </div>
+              
             </div>
         </div>
         <div className="showcase">
