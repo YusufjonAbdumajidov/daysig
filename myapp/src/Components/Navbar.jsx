13d4721d827e75f1ca57c536b0e850ac582e4ad5
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-07-05 12:03:27 +0300

    fixing the navbar

--- myapp/src/Components/Navbar.jsx
+++ myapp/src/Components/Navbar.jsx
@@ -11,14 +11,29 @@ const Navbar = () => {
             <ul>
                 <li>about</li>
                 <li>signals</li>
-                <li onClick={() => setShowDropdown(!showDropdown)} >more {showDropdown ? <BsChevronUp /> : <BsChevronDown />  }  </li>
-                <ul className={showDropdown ? "dropDown showDropDown" : 'dropDown'}>
+                {/* <li onClick={() => setShowDropdown(!showDropdown)} >more {showDropdown ? <BsChevronUp /> : <BsChevronDown />  }  </li> */}
+                {/* <ul className={showDropdown ? "dropDown showDropDown" : 'dropDown'}>
                   <li>registration</li>
                   <li>market</li>
                   <li>community</li>
                   <li>resources</li>
-                </ul>
-                <li onClick={() => setChangeLanguage(!changeLanguage)} ><BsGlobe2 style={{marginRight: "6px"}}/> {changeLanguage ? "Ru" : "Eng"}</li>
+                </ul> */}
+                
+
+                <div class="dropdown">
+                  {/* <span>More</span> */}
+                  <li onClick={() => setShowDropdown(!showDropdown)} >more   </li>
+                  <div class="dropdown-content">
+                    <li>registration</li>
+                    <li>market</li>
+                    <li>community</li>
+                    <li>resources</li>
+                    <li onClick={() => setChangeLanguage(!changeLanguage)} ><BsGlobe2 style={{marginRight: "6px"}}/> {changeLanguage ? "Ru" : "Eng"}</li>
+                  </div>
+                </div>
+
+                
+                {/* <li onClick={() => setChangeLanguage(!changeLanguage)} ><BsGlobe2 style={{marginRight: "6px"}}/> {changeLanguage ? "Ru" : "Eng"}</li> */}
             </ul>
         </nav>
 
