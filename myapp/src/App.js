2fe7d5d35608a55ef57834e305303d60e5529377
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-07-13 10:51:21 +0300

    new changes made

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,14 @@
 import "./index.scss";
 import Navbar from "./Components/Navbar";
 import Main from "./Components/Main";
+import About from "./Components/About";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Main />
+      <About />
     </div>
   );
 }
--- myapp/src/Components/About.jsx
+++ myapp/src/Components/About.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const About = () => {
   return (
     <div className='aboutContainer'>
-        
+        <section>
+            <h1>Our best users are all over the world with full pocket of money</h1>
+            <p>Our platfrom reaches people all over the world, hereby we are trusted as the best platfrom for binary trading!</p>
+        </section>
+        <section></section>
     </div>
   )
 }
