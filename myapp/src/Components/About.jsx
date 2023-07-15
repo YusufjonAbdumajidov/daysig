c70dba1a8252a5517c26114005101879c3ec2870
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-07-15 11:28:37 +0300

    placing a map in about section

--- myapp/src/Components/About.jsx
+++ myapp/src/Components/About.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
+import mapImage from "../assets/map4.png"
 
 const About = () => {
   return (
     <div className='aboutContainer'>
         <section>
-            <h1>Our best users are all over the world with full pocket of money</h1>
-            <p>Our platfrom reaches people all over the world, hereby we are trusted as the best platfrom for binary trading!</p>
+          <h1>Our best users are all over the world with full pocket of money</h1>
+          <p>Our platfrom reaches people all over the world, hereby we are trusted as the best platfrom for binary trading!</p>
         </section>
-        <section></section>
+        <section>
+          <img src={mapImage} alt="" />
+        </section>
     </div>
   )
 }
