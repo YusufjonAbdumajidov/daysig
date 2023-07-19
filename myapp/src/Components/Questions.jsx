b354f5dd8628d403f090ad38b48bd96d1e6f4155
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-07-19 13:25:34 +0300

    final touched done

--- myapp/src/Components/Questions.jsx
+++ myapp/src/Components/Questions.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Questions = () => {
   return (
-    <div>Questions</div>
+    <div className='questionsContainer'>
+        <section>
+            <h1>Frequently Asked Questions</h1>
+            <p>Have a question that is not answered? Contact us at example@gmail.com</p>
+        </section>
+        <section></section>
+    </div>
   )
 }
 
