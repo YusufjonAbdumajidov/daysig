171b9354417582b6543231f5617d73a5971339dc
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-07-19 13:17:15 +0300

    Setting up new page

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./Components/Navbar";
 import Main from "./Components/Main";
 import About from "./Components/About";
 import Quality from "./Components/Quality";
+import Questions from "./Components/Questions";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Main />
       <About />
       <Quality />
+      <Questions />
     </div>
   );
 }
