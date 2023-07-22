31cca344af09a393bd273755fa25b43a258a3129
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-07-22 10:29:48 +0300

    working on register page

--- myapp/src/Components/Register.jsx
+++ myapp/src/Components/Register.jsx
@@ -5,6 +5,13 @@ const Register = () => {
       <nav>
         <div>daysig.</div>
       </nav>
+      <div className="mainRegister">
+        <section>
+          <h1>Welcome to daysig.</h1> 
+          <p>Anyone can earn their first dollar online. Start with what you know and have. Repeat after our experienced traders and make money. It's that easy.</p>
+        </section>
+        <section></section>
+      </div>
   </div>
 }
 
