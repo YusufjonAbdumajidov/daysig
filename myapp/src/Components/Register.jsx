a6da5dfa81caa67c901f99bad35804d1c08502ea
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-07-24 13:00:15 +0300

    importing images

--- myapp/src/Components/Register.jsx
+++ myapp/src/Components/Register.jsx
@@ -12,6 +12,9 @@ const Register = () => {
         <section>
           <h1>Welcome to daysig.</h1> 
           <p>Anyone can earn their first dollar online. Start with what you know and have. Repeat after our experienced traders and make money. It's that easy.</p>
+          <div className="quote">
+            <p>There is really nothing like trading. Once you crack the code it is like having an ATM on your computer.</p>
+          </div>
         </section>
         <section>
         <form action="#">
