dae24af0c43750e5e848fa6c53789409fd57aa04
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-07-31 11:41:37 +0300

    finishing the d=fundamentals of info page

--- myapp/src/Components/Info.jsx
+++ myapp/src/Components/Info.jsx
@@ -8,23 +8,24 @@ const Info = () => {
   return (
     <div className='infoContainer'>
       <section>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam rem sint temporibus eaque quae fuga numquam rerum, quas voluptate atque, itaque cupiditate voluptates minus architecto ipsa expedita. Eveniet, quibusdam quisquam!
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam rem sint temporibus eaque quae fuga numquam rerum, quas voluptate atque, itaque cupiditate voluptates minus architecto ipsa expedita. Eveniet, quibusdam quisquam!
+        </p>
       </section>
       <section>
         <div>
           <img src={trader1} alt="" />
-        </div>
-        <div>
           <img src={trader2} alt="" />
-          <img src={trader3} alt="" />
         </div>
         <div>
-          <img src={trader4} alt="" />
-        </div>
-        
+          <img src={trader3} alt="" />
+          <img src={trader4} alt="" /> 
+        </div> 
       </section>
       <section>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi sequi cumque pariatur consequatur asperiores deleniti illo, reiciendis est sapiente nemo. Non quaerat, fugit animi hic necessitatibus mollitia voluptatem! Nesciunt, provident.
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam rem sint temporibus eaque quae fuga numquam rerum, quas voluptate atque, itaque cupiditate voluptates minus architecto ipsa expedita. Eveniet, quibusdam quisquam!
+        </p>
       </section>
     </div>
   )
