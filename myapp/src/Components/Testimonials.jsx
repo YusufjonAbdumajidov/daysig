670a914020eda208aedf99ecdd2a9aee263d5544
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-08-16 10:38:16 +0300

    design changes

--- myapp/src/Components/Testimonials.jsx
+++ myapp/src/Components/Testimonials.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Testimonials = () => {
   return (
-    <div className='testimonies_container'>
-        <h1>Hear from real clients and traders about their experience with our signals</h1>
-    </div>
+    // <div className='testimonies_container'>
+    //     <h1>Hear from real clients and traders about their experience with our signals</h1>
+    // </div>
   )
 }
 
