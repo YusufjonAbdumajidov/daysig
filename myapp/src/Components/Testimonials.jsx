708344341f217531278fd07c884ff7f8894384c6
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-08-18 10:59:31 +0300

    working on testimonials

--- myapp/src/Components/Testimonials.jsx
+++ myapp/src/Components/Testimonials.jsx
@@ -4,6 +4,14 @@ const Testimonials = () => {
   return (
     <div className='testimonies_container'>
         <h1>Hear from real clients and traders about their experience with our signals</h1>
+        <div>
+          <section>
+            <div>user 1</div>
+            <div>user 2</div>
+          </section>
+          <section></section>
+        </div>
+       
     </div>
   )
 }
