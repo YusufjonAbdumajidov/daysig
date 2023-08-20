7e6772998e47c55f85b7de88da4b554b632662da
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-08-20 11:51:58 +0300

    working on testimonials

--- myapp/src/Components/Testimonials.jsx
+++ myapp/src/Components/Testimonials.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import trader1 from "../assets/trader1.jpg";
 import trader2 from "../assets/trader1.jpg";
 import trader3 from "../assets/trader1.jpg";
+import { LiaStarSolid } from "react-icons/lia";
 
 const Testimonials = () => {
   return (
@@ -11,12 +12,39 @@ const Testimonials = () => {
           <section>
             <div>
               <img src={trader1} alt="" />
+              <h3>Paul</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, provident!</p>
+              <span className="ratingStars">
+                < LiaStarSolid />
+                < LiaStarSolid />
+                < LiaStarSolid />
+                < LiaStarSolid />
+                < LiaStarSolid />
+              </span>
             </div>
             <div>
               <img src={trader2} alt="" />
+              <h3>Paul</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, provident!</p>
+              <span className="ratingStars">
+                < LiaStarSolid />
+                < LiaStarSolid />
+                < LiaStarSolid />
+                < LiaStarSolid />
+                < LiaStarSolid />
+              </span>
             </div>
             <div>
               <img src={trader3} alt="" />
+              <h3>Paul</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, provident!</p>
+              <span className="ratingStars">
+                < LiaStarSolid />
+                < LiaStarSolid />
+                < LiaStarSolid />
+                < LiaStarSolid />
+                < LiaStarSolid />
+              </span>
             </div>
           </section>
           <section></section>
