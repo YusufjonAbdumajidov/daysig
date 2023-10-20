570fa4f09cc24d0c9b7709a58673d28813448c4d
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-10-20 21:06:26 +0300

    changing background color

--- myapp/src/Components/Questions.jsx
+++ myapp/src/Components/Questions.jsx
@@ -4,8 +4,8 @@ const Questions = () => {
   return (
     <div className='questionsContainer'>
         <section>
-            <h1>Frequently Asked Questions</h1>
-            <p>Have a question that is not answered? Contact us at example@gmail.com</p>
+          <h1>Frequently Asked Questions</h1>
+          <p>Have a question that is not answered? Contact us at example@gmail.com</p>
         </section>
         <section></section>
     </div>
