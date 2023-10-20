33f80211adc8f78c0c24e5b8c3d6ca0c76cf35ab
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-10-20 21:26:18 +0300

    new design changes added

--- myapp/src/Components/Questions.jsx
+++ myapp/src/Components/Questions.jsx
@@ -7,7 +7,14 @@ const Questions = () => {
           <h1>Frequently Asked Questions</h1>
           <p>Have a question that is not answered? Contact us at example@gmail.com</p>
         </section>
-        <section></section>
+        <section>
+          <div>1 question</div>
+          <div>2 question</div>
+          <div>3 question</div>
+          <div>4 question</div>
+          <div>5 question</div>
+          <div>6 question</div>
+        </section>
     </div>
   )
 }
