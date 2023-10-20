f8081feae10023154b5ab0f0052c8377de2ad63b
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-10-20 22:22:17 +0300

    new changes added

--- myapp/src/Components/Questions.jsx
+++ myapp/src/Components/Questions.jsx
@@ -12,12 +12,19 @@ const Questions = () => {
           <p>Have a question that is not answered? Contact us at example@gmail.com</p>
         </section>
         <section>
-          <div onClick={() => setChangeIcon(!changeIcon)} >1 question <BsChevronDown className='downSide' /> </div>
-          <div>2 question <BsChevronDown className='downSide' /></div>
-          <div>3 question <BsChevronDown className='downSide' /></div>
-          <div>4 question <BsChevronDown className='downSide' /></div>
-          <div>5 question <BsChevronDown className='downSide' /></div>
-          <div>6 question <BsChevronDown className='downSide' /></div>
+          <div onClick={() => setChangeIcon(!changeIcon)} >
+            <section> 1 question 
+              {changeIcon ? <BsChevronDown className='downSide'/>  : <BsChevronUp className='downSide'/>}
+            </section>
+            <section className="answer">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, pariatur. Reiciendis dignissimos minima ex voluptas earum sapiente cumque consectetur laudantium esse dolorem vero officiis ipsum et, architecto voluptates harum vitae.</p>
+            </section>
+          </div>
+          <div onClick={() => setChangeIcon(!changeIcon)} ><section> 2 question <BsChevronDown className='downSide' /></section> </div>
+          <div onClick={() => setChangeIcon(!changeIcon)} ><section> 3 question <BsChevronDown className='downSide' /></section> </div>
+          <div onClick={() => setChangeIcon(!changeIcon)} ><section> 4 question <BsChevronDown className='downSide' /></section> </div>
+          <div onClick={() => setChangeIcon(!changeIcon)} ><section> 5 question <BsChevronDown className='downSide' /></section> </div>
+          <div onClick={() => setChangeIcon(!changeIcon)} ><section> 6 question <BsChevronDown className='downSide' /></section> </div>
         </section>
     </div>
   )
