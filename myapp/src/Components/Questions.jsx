ab0ef91fd1491bbcf538ba5d88663c4630d6702e
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-10-22 01:04:30 +0300

    working on the questions section

--- myapp/src/Components/Questions.jsx
+++ myapp/src/Components/Questions.jsx
@@ -4,7 +4,37 @@ import {  BsChevronDown, BsChevronUp } from "react-icons/bs";
 
 const Questions = () => {
   const [changeIcon, setChangeIcon] = useState(false);
-  const [openQuestion, setOpenQuestion] = useState(false);
+  let questions = {
+    firstQuestion: [
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, pariatur. Reiciendis dignissimos minima ex voluptas earum sapiente cumque consectetur laudantium esse dolorem vero officiis ipsum et, architecto voluptates harum vitae." , false
+    ],
+    secondQuestion: [
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, pariatur. Reiciendis dignissimos minima ex voluptas earum sapiente cumque consectetur laudantium esse dolorem vero officiis ipsum et, architecto voluptates harum vitae." 
+    ],
+    thirdQuestion: [
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, pariatur. Reiciendis dignissimos minima ex voluptas earum sapiente cumque consectetur laudantium esse dolorem vero officiis ipsum et, architecto voluptates harum vitae." 
+    ],
+    fourthQuestion: [
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, pariatur. Reiciendis dignissimos minima ex voluptas earum sapiente cumque consectetur laudantium esse dolorem vero officiis ipsum et, architecto voluptates harum vitae." 
+    ],
+    fifthQuestion: [
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, pariatur. Reiciendis dignissimos minima ex voluptas earum sapiente cumque consectetur laudantium esse dolorem vero officiis ipsum et, architecto voluptates harum vitae."
+    ],
+    sixthQuestion: [
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, pariatur. Reiciendis dignissimos minima ex voluptas earum sapiente cumque consectetur laudantium esse dolorem vero officiis ipsum et, architecto voluptates harum vitae." 
+    ]
+  }
+
+  const [ showAnswer, setShowAnswer ] = useState({
+    firstAnswer: false,
+    secondAnswer: false,
+    thirdAnswer: false,
+    fourthAnswer: false,
+    fifthAnswer: false,
+    sixthAnswer: false,
+  });
+
+  console.log(showAnswer.firstAnswer)
 
   return (
     <div className='questionsContainer'>
@@ -13,21 +43,23 @@ const Questions = () => {
           <p>Have a question that is not answered? Contact us at example@gmail.com</p>
         </section>
         <section>
-          <div onClick={() => {setOpenQuestion(!openQuestion); setChangeIcon(!changeIcon)}} >
+          <div onClick={() => { setShowAnswer({...showAnswer, firstAnswer: !showAnswer.firstAnswer}) ; setChangeIcon(!changeIcon)}} >
+
             <section> 1 question 
               {changeIcon ? <BsChevronDown className='downSide'/>  : <BsChevronUp className='downSide'/>}
             </section>
-            {openQuestion ? 
+           
             <section className="answer">
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, pariatur. Reiciendis dignissimos minima ex voluptas earum sapiente cumque consectetur laudantium esse dolorem vero officiis ipsum et, architecto voluptates harum vitae.</p>
-            </section> : ""}
+              <p>{showAnswer.firstAnswer ? questions.firstQuestion[0] : ""}</p>
+            </section>
+
           </div>
-          <div onClick={() => {setOpenQuestion(!openQuestion); setChangeIcon(!changeIcon)}} >
+          <div onClick={() => { setChangeIcon(!changeIcon)}} >
             <section> 2 question <BsChevronDown className='downSide' /></section>
-            {openQuestion ? 
+            
             <section className="answer">
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, pariatur. Reiciendis dignissimos minima ex voluptas earum sapiente cumque consectetur laudantium esse dolorem vero officiis ipsum et, architecto voluptates harum vitae.</p>
-            </section> : ""}
+              <p></p>
+            </section>
           </div>
           <div onClick={() => setChangeIcon(!changeIcon)} ><section> 3 question <BsChevronDown className='downSide' /></section></div>
           <div onClick={() => setChangeIcon(!changeIcon)} ><section> 4 question <BsChevronDown className='downSide' /></section> </div>
