720107efcc9b1c4ee20ca27b90ca58dd2cf17486
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-10-22 00:23:56 +0300

    working on questions section

--- myapp/src/Components/Questions.jsx
+++ myapp/src/Components/Questions.jsx
@@ -4,7 +4,8 @@ import {  BsChevronDown, BsChevronUp } from "react-icons/bs";
 
 const Questions = () => {
   const [changeIcon, setChangeIcon] = useState(false);
-  console.log(changeIcon);
+  const [openQuestion, setOpenQuestion] = useState(false);
+
   return (
     <div className='questionsContainer'>
         <section>
@@ -12,16 +13,23 @@ const Questions = () => {
           <p>Have a question that is not answered? Contact us at example@gmail.com</p>
         </section>
         <section>
-          <div onClick={() => setChangeIcon(!changeIcon)} >
+          <div onClick={() => {setOpenQuestion(!openQuestion); setChangeIcon(!changeIcon)}} >
             <section> 1 question 
               {changeIcon ? <BsChevronDown className='downSide'/>  : <BsChevronUp className='downSide'/>}
             </section>
+            {openQuestion ? 
             <section className="answer">
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, pariatur. Reiciendis dignissimos minima ex voluptas earum sapiente cumque consectetur laudantium esse dolorem vero officiis ipsum et, architecto voluptates harum vitae.</p>
-            </section>
+            </section> : ""}
           </div>
-          <div onClick={() => setChangeIcon(!changeIcon)} ><section> 2 question <BsChevronDown className='downSide' /></section> </div>
-          <div onClick={() => setChangeIcon(!changeIcon)} ><section> 3 question <BsChevronDown className='downSide' /></section> </div>
+          <div onClick={() => {setOpenQuestion(!openQuestion); setChangeIcon(!changeIcon)}} >
+            <section> 2 question <BsChevronDown className='downSide' /></section>
+            {openQuestion ? 
+            <section className="answer">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, pariatur. Reiciendis dignissimos minima ex voluptas earum sapiente cumque consectetur laudantium esse dolorem vero officiis ipsum et, architecto voluptates harum vitae.</p>
+            </section> : ""}
+          </div>
+          <div onClick={() => setChangeIcon(!changeIcon)} ><section> 3 question <BsChevronDown className='downSide' /></section></div>
           <div onClick={() => setChangeIcon(!changeIcon)} ><section> 4 question <BsChevronDown className='downSide' /></section> </div>
           <div onClick={() => setChangeIcon(!changeIcon)} ><section> 5 question <BsChevronDown className='downSide' /></section> </div>
           <div onClick={() => setChangeIcon(!changeIcon)} ><section> 6 question <BsChevronDown className='downSide' /></section> </div>
