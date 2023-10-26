b0adeb583a9f6dc4bd64e29ab846f0cdc01db5bb
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-10-26 16:04:15 +0200

    new changes added

--- myapp/src/Components/Footer.jsx
+++ myapp/src/Components/Footer.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { BsInstagram } from  "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className='footerContainer' >
       <section>
+        <ul>
         <h1>DAYSIG</h1>
         <p>Our team of seasoned traders and cutting-edge algorithms analyze market trends and indicators to deliver accurate signals in real-time.</p>
+        </ul>
+        
       </section>
       <section>
         <ul>
@@ -20,12 +26,17 @@ const Footer = () => {
       </section>
       <section>
         <ul>
-        <h2>Address</h2>
+          <h2>Address</h2>
           <li>New York</li>
           <li>Wolf street</li>
           <li>+39 292 292 999</li>
           <li>est@gmail.com</li>
         </ul>
+        <ol>
+          <li>< BsInstagram /></li>
+          <li>< AiOutlineMail /></li>
+          <li>< FaFacebook /></li>
+        </ol>
       </section>
     </div>
   )
