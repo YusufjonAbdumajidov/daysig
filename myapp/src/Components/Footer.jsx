fb71dc5ed227cd9f1aba1bbfd76c2d3e8c401b6c
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-10-26 15:32:27 +0200

    new links added to the footer section

--- myapp/src/Components/Footer.jsx
+++ myapp/src/Components/Footer.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='footerContainer' >Copyright is reserved</div>
+    <div className='footerContainer' >
+      <section>
+        <h1>DAYSIG</h1> 
+      </section>
+      <section>
+        <ul>
+          <li>About</li>
+          <li>Signals</li>
+          <li>Registraion</li>
+          <li>Market</li>
+          <li>Community</li>
+          <li>Resources</li>
+        </ul>
+      </section>
+      <section>
+        <h2>New York</h2>
+        <p>Wolf street</p>
+        <p>+39 292 292 999</p>
+        <p>test@gmail.com</p>
+      </section>
+    </div>
   )
 }
 
