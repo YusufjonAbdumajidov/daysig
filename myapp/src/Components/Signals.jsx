dabcd69cf8981d7d04dc72b776267dc5478b8804
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-10-30 14:33:28 +0100

    creating the structure of signals section

--- myapp/src/Components/Signals.jsx
+++ myapp/src/Components/Signals.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
 
 const Signals = () => {
-  return (
-    <div className='signalsContainer'>Signals</div>
-  )
+  return <>
+    <div className='signalsContainer'>
+        <div>
+            <header>
+                <section>
+                    <h2>Snap</h2>
+                    <p>Stock</p>
+                </section>
+                <section>
+                    <button>Long</button>
+                    <button>Short</button>
+                </section>
+            </header>
+            <main>
+                <section>
+                    <p>Entry Date/Time:</p>
+                    <span>08-12-21 9:40 AM</span>
+                    <p>Position:</p>
+                    <span>$200 x 1.200</span>
+                </section>
+                <section>
+                    <p>Exit Date/Time:</p>
+                    <span>08-12-21 9:40 AM</span>
+                    <p>Profit:</p>
+                    <span>up $200  down 20%</span>
+                </section>
+            </main>
+        </div>
+        <div>
+            <header>
+                <section>
+                    <h2>Snap</h2>
+                    <p>Stock</p>
+                </section>
+                <section>
+                    <button>Long</button>
+                    <button>Short</button>
+                </section>
+            </header>
+            <main>
+                <section>
+                    <p>Entry Date/Time:</p>
+                    <span>08-12-21 9:40 AM</span>
+                    <p>Position:</p>
+                    <span>$200 x 1.200</span>
+                </section>
+                <section>
+                    <p>Exit Date/Time:</p>
+                    <span>08-12-21 9:40 AM</span>
+                    <p>Profit:</p>
+                    <span>up $200  down 20%</span>
+                </section>
+            </main>
+        </div>
+    </div>
+    </>
 }
 
 export default Signals
