53091b99a6a9a0cd0e96a9fc22bf7adc7de2af66
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-11-07 07:23:07 +0100

    added new elements

--- myapp/src/Components/Signals.jsx
+++ myapp/src/Components/Signals.jsx
@@ -33,6 +33,32 @@ const Signals = () => {
         <div>
             <header>
                 <section>
+                    <h2>Bitcoin</h2>
+                    <p>crypto</p>
+                </section>
+                <section>
+                    <button>Long</button>
+                    <button>Short</button>
+                </section>
+            </header>
+            <main>
+                <section>
+                    <p>Entry Date/Time:</p>
+                    <span>08-12-21 9:40 AM</span>
+                    <p>Position:</p>
+                    <span>$200 x 1.200</span>
+                </section>
+                <section>
+                    <p>Exit Date/Time:</p>
+                    <span>08-12-21 9:40 AM</span>
+                    <p>Profit:</p>
+                    <span><AiFillCaretUp className='upIcon' /> $200  <AiFillCaretDown className='downIcon'  />  20%</span>
+                </section>
+            </main>
+        </div>
+        <div>
+            <header>
+                <section>
                     <h2>Etharium</h2>
                     <p>crypto</p>
                 </section>
@@ -79,6 +105,138 @@ const Signals = () => {
                     <span>08-12-21 9:40 AM</span>
                     <p>Profit:</p>
                     <span><AiFillCaretUp className='upIcon' /> $200  <AiFillCaretDown className='downIcon'  />  20%</span>
+                </section>
+            </main>
+        </div>
+        <div>
+            <header>
+                <section>
+                    <h2>Etharium</h2>
+                    <p>crypto</p>
+                </section>
+                <section>
+                    <button>Long</button>
+                    <button>Short</button>
+                </section>
+            </header>
+            <main>
+                <section>
+                    <p>Entry Date/Time:</p>
+                    <span>08-12-21 9:40 AM</span>
+                    <p>Position:</p>
+                    <span>$200 x 1.200</span>
+                </section>
+                <section>
+                    <p>Exit Date/Time:</p>
+                    <span>08-12-21 9:40 AM</span>
+                    <p>Profit:</p>
+                    <span><AiFillCaretUp className='upIcon' /> $200  <AiFillCaretDown className='downIcon' /> 20%</span>
+                </section>
+            </main>
+        </div>
+
+
+        <div>
+            <header>
+                <section>
+                    <h2>Etharium</h2>
+                    <p>crypto</p>
+                </section>
+                <section>
+                    <button>Long</button>
+                    <button>Short</button>
+                </section>
+            </header>
+            <main>
+                <section>
+                    <p>Entry Date/Time:</p>
+                    <span>08-12-21 9:40 AM</span>
+                    <p>Position:</p>
+                    <span>$200 x 1.200</span>
+                </section>
+                <section>
+                    <p>Exit Date/Time:</p>
+                    <span>08-12-21 9:40 AM</span>
+                    <p>Profit:</p>
+                    <span><AiFillCaretUp className='upIcon' /> $200  <AiFillCaretDown className='downIcon' /> 20%</span>
+                </section>
+            </main>
+        </div>
+        <div>
+            <header>
+                <section>
+                    <h2>Snap</h2>
+                    <p>Stock</p>
+                </section>
+                <section>
+                    <button>Long</button>
+                    <button>Short</button>
+                </section>
+            </header>
+            <main>
+                <section>
+                    <p>Entry Date/Time:</p>
+                    <span>08-12-21 9:40 AM</span>
+                    <p>Position:</p>
+                    <span>$200 x 1.200</span>
+                </section>
+                <section>
+                    <p>Exit Date/Time:</p>
+                    <span>08-12-21 9:40 AM</span>
+                    <p>Profit:</p>
+                    <span><AiFillCaretUp className='upIcon' /> $200  <AiFillCaretDown className='downIcon'  />  20%</span>
+                </section>
+            </main>
+        </div>
+        <div>
+            <header>
+                <section>
+                    <h2>Etharium</h2>
+                    <p>crypto</p>
+                </section>
+                <section>
+                    <button>Long</button>
+                    <button>Short</button>
+                </section>
+            </header>
+            <main>
+                <section>
+                    <p>Entry Date/Time:</p>
+                    <span>08-12-21 9:40 AM</span>
+                    <p>Position:</p>
+                    <span>$200 x 1.200</span>
+                </section>
+                <section>
+                    <p>Exit Date/Time:</p>
+                    <span>08-12-21 9:40 AM</span>
+                    <p>Profit:</p>
+                    <span><AiFillCaretUp className='upIcon' /> $200  <AiFillCaretDown className='downIcon' /> 20%</span>
+                </section>
+            </main>
+        </div>
+        <div>
+            <header>
+                <section>
+                    <h2>Etharium</h2>
+                    <p>crypto</p>
+                </section>
+                <section>
+                    <button>Long</button>
+                    <button>Short</button>
+                </section>
+            </header>
+            <main>
+                <section>
+                    <p>Entry Date/Time:</p>
+                    <span>08-12-21 9:40 AM</span>
+                    <p>Position:</p>
+                    <span>$200 x 1.200</span>
+                </section>
+                <section>
+                    <p>Exit Date/Time:</p>
+                    <span>08-12-21 9:40 AM</span>
+                    <p>Profit:</p>
+                    <span><AiFillCaretUp className='upIcon' /> $200  <AiFillCaretDown className='downIcon' /> 20%</span>
                 </section>
             </main>
         </div>
